3c75c51e558379ee80475d97a62dff3a665bbbdc
ANILA <160001699+ANILAALLAHRAKHA@users.noreply.github.com>
2024-02-22 21:24:50 +0500

    complete assig

---
+++ app.js
@@ -0,0 +1,82 @@
+// // chapter1
+// // Q1
+// alert("Error place enter a vaild password.")
+// // Q2
+// alert("welcome to js land..\n happy coding")
+// // Q3
+// alert("welcome to js land..")
+// // Q4
+// alert("Hello.. i can run js through my web browers console.")
+// chapter2 variables and spring
+// Q1
+// var username = ("ANILA");
+// console.log(username)
+// // Q2
+// var myname =("ANILA ALLAH RAKHA")
+// console.log(typeof myname)
+// // Q3
+// var message = ("Hello world")
+// console.log (message)
+
+// var work =("<h1><i></i>HELLO WORLD</h1>")
+// document.write(work)
+// // Q4
+// var name =( "jhone Doe")
+// alert(name)
+// var age =("15 years old")
+// alert(age)
+// var edu =("certified mobile application development")
+// alert(edu)
+// // Q5
+// var piz =("pizza \n pizz \n piz \n pi \n p")
+// Q6
+
+// alert("my email adress is example@example.com" )
+// // Q7
+
+// var A =("I am trying to learn JS BOOK A smarter\n way to javascrip")
+// alert(A)
+// // Q8
+// document.write("Yah! I can write HTML content through Javascrip")
+// // Q9 
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// // chapter2 variables and number
+// // Q1
+// var age  =("i am 15 years old")
+// alert(age)
+// // Q2
+// var site =("“You have visited this site 15 times”.")
+// alert(site)
+// Q3
+var birth =("my <i><\i>birth years is")
+document.write(birth)
+// Q4
+
+    // Prompt user for information
+    var visitorName = ("john Doe");
+    var productTitle = ("on XYZ Clothing store");
+    var quantity =("Order 5 T-shirt's");
+
+    // Display the message in the browser
+    document.write("<p>" + visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.</p>");
+  // // chapter1-3(alert)
+// 1. 
+// I. 
+alert("John");
+// II. 
+alert("Doe");
+// III. 
+alert("john.doe@example.com");
+// IV. 
+alert("123-456-7890");
+// V. 
+alert("mypassword123");
+  // // chapter2-3(variables for string)
+var a = ("anila")
+console.log(typeof a)
+
+
+
+
+
+
